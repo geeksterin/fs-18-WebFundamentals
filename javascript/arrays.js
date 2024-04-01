91608dfa5ab218a9dfb6ae80beca11c772398c16
rohitjainfsl <rohitjainwebdev@gmail.com>
2024-04-01 22:12:05 +0530

    js class 3

---
+++ javascript/arrays.js
@@ -0,0 +1,130 @@
+// Array: Single variable, having multiple values
+
+// let n = "Shashank";
+
+// n = "Abhinandan";
+
+// n = "Nawab";
+
+// console.log(n);
+
+//IN C, C++, JAVA arrays were homogenous
+//IN JS arrays were heterogenous
+
+// let names = new Array();
+
+// let names = [100, 45, "Rohit", false, 100.6];
+
+// let numbers = [3, 4, 5, 6, 7];
+
+// console.log(numbers[0]);
+
+// console.log(numbers[1] + numbers[0]); //7
+
+// console.log(numbers.length); // number of elements in the array
+
+// console.log(numbers[2] + numbers[3] + numbers[numbers.length - 1]);
+
+// push, pop, shift, unshift
+
+// let numbers = [3, 4, 5, 6, 7];
+
+// adds values to the end of the array
+// numbers.push(8, 9, 10);
+
+// adds values to the beginning of the array
+// numbers.unshift(8, 9, 10);
+
+// // removes 1 value from the end of the array
+
+// numbers.pop();
+// numbers.pop();
+// numbers.pop();
+
+// removes 1 value from the beginning of the array
+
+// numbers.shift();
+// numbers.shift();
+// numbers.shift();
+
+// console.log(numbers);
+
+// let numbers = [3, 4, 5, 6, 7];
+
+// console.log(numbers.push(10, 11,12,13))
+
+// console.log(numbers.unshift(10, 12, 13));
+
+// console.log(numbers.pop())
+
+// console.log(numbers.shift())
+
+// let a = numbers.pop(); //7
+// let b = numbers.unshift(a); //5
+// let c = numbers.push(10, 11);  //7
+// console.log(a + b + c);
+
+// let numbers = [3, 4, 5, 6, 7, 12, 45, 11, 78, 96];
+
+//slice
+// cuts the array into smaller array: omits the last index
+// console.log(numbers.slice(0,3))
+
+// if single argument is passed, cuts out starting from that index and rest of the array
+// console.log(numbers.slice(1))
+
+// console.log(numbers.slice(-6, -2));
+// console.log(numbers.slice(-2, -6));
+
+let numbers = [3, 4, 5, 6, 7, 12, 45, 11, 78, 96];
+
+// numbers.splice(startingIndex, deleteCount, elementsToAdd)
+
+// numbers.splice(6, 1, 13)
+// console.log(numbers)
+
+// [1,2,3,4, 7, 12, 45, 11, 78, 96]
+
+// numbers.splice(0, 4, 1, 2, 3, 4);
+
+// numbers.splice(2,2)
+// numbers.splice(0,0,1,2)
+// console.log(numbers);
+
+// Introducing Arrays
+// Difference between Arrays Concept in Java and Javascript
+// length Property
+// Array Methods like
+// toString()
+// join and split
+// push()
+// pop()
+// shift()
+// unshift()
+// Slice()
+// Splice()
+// reverse()
+// some()
+// every()
+// find()
+// delete()
+// sort(), sort with comparator
+
+// const names = ["Ram", "Shyam", "Seeta", "Geeta"]
+// names.reverse()
+// console.log(names);
+
+// const shaktimaan = ["Gangadhar", "Vidhyadhar", "Shastri"];
+
+//toString & join: joins together or binds together the values of an array
+
+
+// console.log(shaktimaan)
+// console.log(shaktimaan.toString())
+
+// console.log(shaktimaan)
+// // console.log(typeof shaktimaan.toString())
+// console.log(shaktimaan.join("-"))
+
+
+
---
+++ javascript/functions2.js
@@ -0,0 +1,71 @@
+// let a = 1;
+// let b = 3;
+// let sum = a + b;
+// console.log(sum);
+
+// let c = 4;
+// let d = 6;
+// let sum1 = c + d;
+// console.log(sum1);
+
+// let e = 4;
+// let f = 8;
+// let sum2 = e + f;
+// console.log(sum2);
+
+// FUNCTION WITH ARGUMENTS
+
+// function sum(a, b) {
+//   let s = a + b;
+//   console.log(s);
+// }
+// sum(1, 3);
+
+// sum(4, 6);
+
+// sum(4, 8);
+
+// sum(10); //Not a Number
+
+// sum(); // NaN
+
+// FUNCTION WITH ARGUMENTS HAVING DEFAULT VALUE
+
+// function sum(a = 5, b = 6, c = 7) {
+//   let s = a + b + c;
+//   console.log(s);
+// }
+// // sum(10, 20); //30
+// // sum(20); //26
+
+// // sum();
+
+// // sum(, 20);
+
+// sum(1, 2, 3);
+
+// FUNCTIONS WITH RETURNING VALUES
+
+// function sum(a, b, c) {
+//   //   let s = a + b + c;
+//   //   return s;
+
+//   return a + b + c;
+// }
+// // let output = sum(3, 4, 5);
+// // console.log(output);
+
+// console.log(sum(3, 4, 5));
+
+// FUNCTIONS ARE FIRST CLASS CITIZENS IN JS
+
+// function sum(a, b, callback) {
+//   //   console.log(callback);
+//   return callback(a, b);
+// }
+
+// function product(a, b) {
+//   return a * b;
+// }
+
+// console.log(sum(3, 4, product));
