ea265da0ee4845536113d962d96d0e0be0a480fe
a.m2002nov <a.m2002nov@gmail.com>
2024-04-04 22:08:04 +0530

    JSON

---
+++ javascript/json.js
@@ -0,0 +1,28 @@
+var user = {
+  name: "Ashish",
+  password: "123456",
+  email: "a.m2002@gmail.com",
+  address: [
+    {
+      state: "Punjab",
+      city: "Nangal",
+      street: "50G Block",
+    },
+    {
+      state: "Bangalroe",
+      city: "Whitefeidl",
+      street: "50G Block",
+    },
+  ],
+};
+console.log(user);
+console.log(typeof user);
+
+user = JSON.stringify(user);
+
+console.log(typeof user);
+console.log(user);
+
+user = JSON.parse(user);
+console.log(typeof user);
+console.log(user);
