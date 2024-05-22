4a3960696283af2f2e846cd544e66403cdf361ba
Ashish Kumar <a.m2002nov@gmail.com>
2024-05-22 21:40:39 +0530

    QNA Session

---
+++ QNA/tdz.js
@@ -0,0 +1,18 @@
+// let const TDZ
+
+// console.log(a)
+// let a = 20;
+
+// let a; // TDZ => Start
+// console.log(a)
+// a = 20; // TDZ END
+
+
+// const func = () => console.log(letVar); 
+// let letVar = 3;
+// func(); 
+
+// let letVar; TDZ
+// const func = () => console.log(letVar); 
+// letVar = 3; TDZ END
+// func(); 
