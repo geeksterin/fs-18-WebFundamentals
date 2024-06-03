ae128d8956457631b1b865ab65651d0d86ea55b4
a.m2002nov <a.m2002nov@gmail.com>
2024-06-03 23:00:32 +0530

    Day 5 Assignment

---
+++ React Assignments/Day5_class_work/src/components/SubjectDetails.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function SubjectDetails({
+  subject,
+  hour,
+  increase,
+  decrease,
+  index,
+}) {
+  return (
+    <div style={{ display: "flex", gap: "10px" }}>
+      <p>{subject}</p>
+      <p>{hour} Hours </p>
+      <button onClick={() => increase(index)}>+</button>
+      <button onClick={() => decrease(index)}>-</button>
+    </div>
+  );
+}
