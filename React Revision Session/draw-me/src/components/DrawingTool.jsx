5da35bd8b40d3ce6fd42e162e0e96ea62be2732d
a.m2002nov <a.m2002nov@gmail.com>
2024-07-20 21:23:50 +0530

    Written funtion to conver SVG to PNG

--- React Revision Session/draw-me/src/components/DrawingTool.jsx
+++ React Revision Session/draw-me/src/components/DrawingTool.jsx
@@ -16,15 +16,36 @@ export default function DrawingTool() {
 function ChildOfTldraw() {
   const editor = useEditor();
 
-  const handleGenerate = () => {
-    const result = editor.getCurrentPageShapeIds();
-    console.log("Seleted Shapes", result);
+  const handleGenerate = async () => {
+    const result = Array.from(editor.getCurrentPageShapeIds());
+    const data = await editor.getSvgString(result);
+    console.log("Seleted Shapes", data);
+    svgToPng(data.svg, data.height, data.width);
   };
+
+  const svgToPng = (svg, height, width) => {
+    // 1. Convert the SVG string to Image Object
+    const img = new Image();
+    const svgBlob = new Blob([svg], { type: "image/svg+xml;charset=utf-8" });
+    const url = URL.createObjectURL(svgBlob);
+    img.src = url;
+
+    img.onload = () => {
+      // once the iamge is loaded successfully. We will create one canvas and inject this image in canvas.
+      const canvas = document.createElement("canvas");
+      canvas.width = width;
+      canvas.height = height;
+      const context = canvas.getContext("2d");
+      context.drawImage(img, 0, 0, width, height);
+
+      // now the image is present on the canvas so we can export that image
+      const pngUrl = canvas.toDataURL("image/png");
+      console.log(pngUrl);
+    };
+  };
+
   return (
-    <button
-      onClick={handleGenerate}
-      style={{ position: "fixed", top: "10px", left: "20px", zIndex: "1000" }}
-    >
+    <button onClick={handleGenerate} className="btn-my">
       Generate Code
     </button>
   );
