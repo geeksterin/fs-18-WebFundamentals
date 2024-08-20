5e90388b287b864a2b6865f742f4b1bb3272f22d
rohitjainfsl <rohit@fullstacklearning.com>
2024-08-20 22:12:53 +0530

    node day 4

---
+++ backend/httpServer/http.js
@@ -0,0 +1,11 @@
+import http from "http";
+
+const data = [
+  { id: 1, name: "Arhan", age: 19 },
+  { id: 2, name: "Nawab", age: 20 },
+  { id: 3, name: "Abhinav", age: 21 },
+  { id: 4, name: "Astha", age: 20 },
+  { id: 5, name: "Rachita", age: 19 },
+];
+
+const server = http.createServer((request, response) => {});
