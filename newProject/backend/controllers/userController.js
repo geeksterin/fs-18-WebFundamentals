a028555e3b374b79ca5f76f49e7740e75a0f07cd
rohitjainfsl <rohit@fullstacklearning.com>
2024-09-05 22:07:34 +0530

    express static

--- newProject/backend/controllers/userController.js
+++ newProject/backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import userModel from "../models/userModel.js";
+import path from "path";
 
 export async function addNewUser(req, res) {
   const { name, email } = req.body;
@@ -11,6 +12,31 @@ export async function addNewUser(req, res) {
 }
 
 export async function getAllUsers(req, res) {
-  const users = await userModel.find();
-  res.send(users);
+  try {
+    const users = await userModel.find();
+    const usersWithPhotos = users.map((user) => {
+      return {
+        // 'uploads\\Kartik Sharma-4cb1f4b6-6d5e-45e4-bcc9-7df95519dc2b.jpeg'
+        ...user._doc,
+        photo: `${req.protocol}://${req.get("host")}/uploads/${path.basename(
+          user.photo
+        )}`,
+      };
+    });
+    res.send(usersWithPhotos);
+  } catch (err) {
+    res.send("Error: " + err);
+  }
 }
+
+
+// AUTHENTICATION: bcrypt(encryption)
+// & AUTHORISATION: (JWT)JSON Web Tokens / Passport
+// PROTECTED ROUTES
+
+
+
+
+// NawabKhan_cuteBoy => e4wsd9_dnsdg3sdr_dst434rasdfsegnkdsgnklsdfnlsdfk123efg
+
+
--- newProject/backend/middlewares/uploadPhoto.js
+++ newProject/backend/middlewares/uploadPhoto.js
@@ -2,14 +2,19 @@ import multer from "multer";
 import path from "path";
 import { v4 as uuidv4 } from "uuid";
 
+// In case of using a function in the destination key: the folder has to be created manually
+
+
 const diskStorageOptions = multer.diskStorage({
   destination: "uploads/",
   filename: function (req, file, callback) {
     const randomString = uuidv4();
     const ext = path.extname(file.originalname);
+    // if(!allowedExtension(ext))  return new Error("")
     const filename = req.body.name + "-" + randomString + ext;
     callback(null, filename);
   },
+  fileSize: 1048576
 });
 
 const uploadPhoto = multer({ storage: diskStorageOptions });
